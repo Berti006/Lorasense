4d9ac6197122cc9e97936c3b8b111d17d35d2c98
Patrick Günzel <p.guenzel@tum.de>
2018-06-16 10:52:07 +0200

    simple wifi scan added

--- wifi_sense.py
+++ wifi_sense.py
@@ -1,6 +1,7 @@
 import pycom
 import network
 from network import LoRa
+from network import WLAN
 import binascii
 
 mac = binascii.hexlify(network.LoRa().mac())
@@ -9,8 +10,15 @@ mac = binascii.hexlify(network.LoRa().mac())
 print("*************************************")
 print("     LoraSense 0.1")
 print("")
-# print("MAC: " + mac)
+print("MAC: " + mac.upper().decode('utf-8'))
 print("*************************************")
 
 pycom.heartbeat(False)
 pycom.rgbled(0x000000)
+
+wifi_adapter = WLAN(mode=WLAN.STA)
+print("Wifi Mode: " + str(wifi_adapter.mode()))
+
+nets = wifi_adapter.scan()
+for net in nets:
+    print(net.ssid)
